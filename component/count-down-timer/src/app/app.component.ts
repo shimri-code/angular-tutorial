cd515cbf83cf8f31a960df39f98657763b6ae2a7
shimri <shimridevsolutions@gmail.com>
2022-05-06 16:33:31 +0300

    modified: 1. two way binding 2. ts/js logic

--- component/count-down-timer/src/app/app.component.ts
+++ component/count-down-timer/src/app/app.component.ts
@@ -6,19 +6,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  timeRemain = '01:00';
-  isStarted = false;
+  timeRemain:string = '01:00';
+  isStarted:boolean = false;
+  currentTime:number = 0;
+  seconds:number = 1;
+  minutes:number = 0;
+  timerId:any;
+
+  secondsUpdate(event:any)
+  {
+    const currentVal:number = parseInt(event.target.value);
+
+    if(!isNaN(currentVal))
+    {
+      this.seconds = parseInt(event.target.value);
+    }
+  }
+
+  minutesUpdate(event:any)
+  {
+    const currentVal:number = parseInt(event.target.value);
+
+    if(!isNaN(currentVal))
+    {
+      this.minutes = parseInt(event.target.value);
+    }
+  }
+
+  calcTime()
+  {
+    const min:number = parseInt((this.currentTime / 60).toString());
+    const sec:number = parseInt((this.currentTime % 60).toString());
+
+    this.timeRemain = min.toString().padStart(2, '0') + ":" + sec.toString().padStart(2, '0');
+  }
 
   startTimer()
   {
-    console.log("Start Clicked");
+    this.currentTime =  this.seconds + this.minutes * 60;
     this.isStarted= true;
+
+    this.timerId = setInterval(()=>{
+      if(this.currentTime > 0)
+      {
+        this.currentTime -=1;
+        this.calcTime();
+      }
+      else
+      {
+        this.stopTimer();
+      }
+    }, 1000);
   }
 
   stopTimer()
   {
-    console.log("Stop Clicked");
     this.isStarted= false;
+    clearInterval(this.timerId);
   }
 
 }
